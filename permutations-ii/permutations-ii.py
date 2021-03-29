cc067e5e7aecf3f2e171c940a06c4f871019cdf9
Vishok Jonnalagadda <49622180+VishokJ@users.noreply.github.com>
2021-03-29 17:01:36 -0500

    Time: 416 ms (19.21%), Space: 14.1 MB (6.12%) - LeetHub

--- permutations-ii/permutations-ii.py
+++ permutations-ii/permutations-ii.py
@@ -1,19 +1,19 @@
 class Solution(object):
     def permuteUnique(self, nums):
         arrList = []
+        final = []
         permList = []
         if len(nums) <= 1: return [nums]
         if len(nums) == 2:
             permList = [[nums[0], nums[1]], [nums[1], nums[0]]]
-            final = []
             [final.append(x) for x in permList if x not in final]
             return final
         for i in range(len(nums)):
-            arrList.append(nums[i:]+nums[0:i])
+            if nums[i:]+nums[0:i] not in arrList:
+                arrList.append(nums[i:]+nums[0:i])
         for i in arrList:
             for x in self.permuteUnique(i[:-1]):
                 x.append(i[-1])
                 permList.append(x)
-        final = []
         [final.append(x) for x in permList if x not in final]
         return final
